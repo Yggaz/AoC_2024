b6e0bc839294d0dab3ffdf510deb05825a582eba
Evgeny Petrov <eupetrov@yandex.ru>
2024-12-16 08:44:01 +0500

    Refactored 2021 Day 11 Part 1 and 2 via list

--- AoC_2021_11_01.py
+++ AoC_2021_11_01.py
@@ -1,21 +1,20 @@
-fish = {}
+fish = []
 for i in range(9):
-    fish[i] = 0
+    fish.append(0)
 for ln in open('input_2021_11.txt', 'r', encoding='utf-8'):
     lin = list(map(int, ln.split(',')))
     for f in lin:
-        fish[f] = fish.get(f, 0) + 1
+        fish[f] += 1
 # N = 80
 N = 256
 for i in range(N):
-    nxt = {}
+    tmp = fish[0]
     for j in range(8):
-        nxt[j] = fish[j+1]
-    nxt[6] += fish[0]
-    nxt[8] = fish[0]
-    fish = nxt
+        fish[j] = fish[j+1]
+    fish[8] = tmp
+    fish[6] += tmp
     if i == 79:
-        s1 = sum(fish[k] for k in fish.keys())
+        s1 = sum(k for k in fish)
         print("Part 1 answer:", s1)
-s2 = sum(fish[k] for k in fish.keys())
+s2 = sum(k for k in fish)
 print("Part 2 answer:", s2)
