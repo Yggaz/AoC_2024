2e805b1a8c4b015ea2ae2c978f9d9964c29ab0a1
Evgeny Petrov <eupetrov@yandex.ru>
2024-12-19 20:15:10 +0500

    Solved Day 19 Part 1 and 2, 38 stars

--- AoC_2024_19_1.py
+++ AoC_2024_19_1.py
@@ -1,4 +1,8 @@
-import heapq
+from copy import copy, deepcopy
+from time import time
+
+
+start_time = time()
 designs = []
 for i, ln in enumerate(open('input_19.txt', 'r', encoding='utf-8')):
     lin = ln.strip()
@@ -8,42 +12,55 @@ for i, ln in enumerate(open('input_19.txt', 'r', encoding='utf-8')):
         if lin:
             designs.append(lin)
 indep = []
+combo = dict()
 for j, d in enumerate(towels):
     solved = False
-    q = [(len(d), d)]
+    combo[d] = []
+    q = [(d, [])]
     cnt = 0
     while q:
-        l, des = heapq.heappop(q)
+        des, sol = q.pop(-1)
         for k, t in enumerate(towels):
             if k == j:
                 continue
             elif t == des:
                 solved = True
-                break
+                solut = deepcopy(sol)
+                solut.append(t)
+                if solut not in combo[d]:
+                    combo[d].append(solut)
             elif t == des[:len(t)]:
-                heapq.heappush(q, (len(des[len(t):]), des[len(t):]))
+                solut = deepcopy(sol)
+                solut.append(t)
+                q.append((des[len(t):], solut))
     if not solved:
         indep.append(d)
+for k in indep:
+    del combo[k]
 good = []
 bad = []
+solutions = dict()
 for j, d in enumerate(designs):
     solved = False
-    q = [(len(d), d)]
-    cnt = 0
+    solutions[d] = []
+    q = [(d, [])]
     while q:
-        l, des = heapq.heappop(q)
+        des, sol = q.pop(-1)
         for t in indep:
             if t == des:
                 solved = True
-                break
+                solut = deepcopy(sol)
+                solut.append(t)
+                if solut not in solutions[d]:
+                    (solutions[d]).append(solut)
             elif t == des[:len(t)]:
-                heapq.heappush(q, (len(des[len(t):]), des[len(t):]))
-        cnt += 1
+                solut = deepcopy(sol)
+                solut.append(t)
+                q.append((des[len(t):], solut))
     if solved:
         good.append(j)
-    else:
-        bad.append(j)
-print(len(good))
-print(good)
-print(len(bad))
+all_solutions = []
+cur_sol = []
 
+print(len(good))
+print("Elapsed time: %s seconds" % round(time() - start_time, 3))
---
+++ AoC_2024_19_2.py
@@ -0,0 +1,30 @@
+from time import time
+from functools import cache
+
+@cache
+def how_many(des):
+    res = 0
+    if not des:
+        return 0
+    for t in towels:
+        if t == des:
+            res += 1
+        elif len(t) < len(des) and t == des[:len(t)]:
+            res += how_many(des[len(t):])
+    return res
+
+start_time = time()
+designs = []
+for i, ln in enumerate(open('input_19.txt', 'r', encoding='utf-8')):
+    lin = ln.strip()
+    if i == 0:
+        towels = list(lin.split(', '))
+    else:
+        if lin:
+            designs.append(lin)
+s = 0
+cnt = 0
+for d in designs:
+    s += how_many(d)
+print("Part 2 answer:", s)
+print("Elapsed time: %s seconds" % round(time() - start_time, 3))
