f950a6c5f2d54c72c59f388e5662bc1ce898bfe8
Evgeny Petrov <eupetrov@yandex.ru>
2024-12-23 10:55:09 +0500

    Solved Day 23 Part 1, 45 stars

---
+++ AoC_2024_23_1.py
@@ -0,0 +1,24 @@
+from itertools import combinations
+data = open('input_23.txt', 'r', encoding='utf-8').read().splitlines()
+connections = dict()
+three = list()
+for ln in data:
+    fir, sec = ln.split('-')
+    if sec not in connections.get(fir,[]):
+        if len(connections.get(fir,[])) == 0:
+            connections[fir] = [sec]
+        else:
+            connections[fir].append(sec)
+    if fir not in connections.get(sec,[]):
+        if len(connections.get(sec,[])) == 0:
+            connections[sec] = [fir]
+        else:
+            connections[sec].append(fir)
+for k in connections.keys():
+    if k[0] == 't':
+        for comps in combinations(connections[k], 2):
+            if comps[0] in connections[comps[1]]:
+                tmp = sorted([k, comps[0], comps[1]])
+                if tmp not in three:
+                    three.append(tmp)
+print('Answer for part 1:', len(three))
